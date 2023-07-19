f5ac7f30e5c2b9d19381b784b0cdd797a64e0df6
Fareen <113833605+ansarifareen@users.noreply.github.com>
2023-07-19 15:06:11 +0530

    Create Search in 2D Matrix.py

---
+++ Day 3/Search in 2D Matrix.py
@@ -0,0 +1,20 @@
+You are given an m x n integer matrix matrix with the following two properties:
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+
+  class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == target: 
+                    return True
+        return False
+
+
+#space Complexity: O(1)
+ #Time Complexity: O(n^2)
+ 
